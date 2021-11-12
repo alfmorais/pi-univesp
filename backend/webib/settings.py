a6e41121102baf38f14707b576c0fb0f9348561c
AlanaFrancino <71753897+AlanaFrancino@users.noreply.github.com>
2021-11-12 20:49:09 -0300

    static

--- backend/webib/settings.py
+++ backend/webib/settings.py
@@ -113,12 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [
-	os.path.join(BASE_DIR, "static_local")
+STATICFILES_DIRS = [
+    'static',
 ]
 
-STATIC_ROOT = "../Font-End/templades/css/dashboard.css"
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
